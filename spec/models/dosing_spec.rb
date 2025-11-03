0cf7f2131cdc0f9ce7917dd9bd8f3486be3134a0
sharkbyte <sgquin@gmail.com>
2025-11-02 23:29:35 -0500

    no annotations in test

--- spec/models/dosing_spec.rb
+++ spec/models/dosing_spec.rb
@@ -17,13 +17,3 @@ RSpec.describe Dosing, type: :model do
     end
   end
 end
-
-# == Schema Information
-#
-# Table name: dosings
-#
-#  id         :bigint           not null, primary key
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  pet_id     :integer          not null
-#
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -5,18 +5,3 @@ RSpec.describe Pet, type: :model do
     expect(create(:pet)).to be_valid
   end
 end
-
-# == Schema Information
-#
-# Table name: pets
-#
-#  id         :bigint           not null, primary key
-#  name       :string           not null
-#  slug       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-# Indexes
-#
-#  index_pets_on_slug  (slug) UNIQUE
-#
